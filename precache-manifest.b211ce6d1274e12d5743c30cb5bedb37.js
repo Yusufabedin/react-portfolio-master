5525452fde5be44059de127e25d0be78755a552b
yusuf <yusuf.abedin95@gmail.com>
2020-12-21 16:26:12 +0600

    Updates

---
+++ precache-manifest.b211ce6d1274e12d5743c30cb5bedb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a94502102661924a377a3d4add2ad71",
+    "url": "/react-portfolio-master/index.html"
+  },
+  {
+    "revision": "a0277e361f945604c9be",
+    "url": "/react-portfolio-master/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/react-portfolio-master/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio-master/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0277e361f945604c9be",
+    "url": "/react-portfolio-master/static/js/main.e1e3fb42.chunk.js"
+  },
+  {
+    "revision": "38a0e517bcfeceb22ec3",
+    "url": "/react-portfolio-master/static/js/runtime-main.bc31d2fb.js"
+  }
+]);
